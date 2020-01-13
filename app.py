8081f91fa03779098f19548ff77cba8d3f0cc3cf
David Mohammadi <dmohamm2@illinois.edu>
2020-01-13 13:29:33 -0600

    Reformatted functions, created a pull_data() function that is called in each callback function.
    Signed-off-by: David Mohammadi <dmohamm2@illinois.edu>

--- app.py
+++ app.py
@@ -13,6 +13,32 @@ from dash.dependencies import Output, Input, State
 
 
 # ===== backend code =====
+def pull_stock_data(input_stock_1, input_stock_2, input_date_start, input_date_end):
+    df_stocks = pd.DataFrame()
+    data_provider = 'yahoo'
+
+    # Inputted from callback
+    list_of_companies = [input_stock_2, input_stock_1]
+    start_date = input_date_start
+    end_date = input_date_end
+
+    for company in list_of_companies:
+        df = DataReader(name=company,
+                        data_source=data_provider,
+                        start=start_date,
+                        end=end_date).reset_index()
+        df["ticker"] = company
+        df_stocks = pd.concat([df_stocks, df])
+
+    df_stocks.reset_index(drop=True, inplace=True)
+
+    df_stock_price = pd.pivot(df_stocks, index='Date', columns='ticker', values='Close')
+    df_stock_price = pd.DataFrame(df_stock_price.to_records())
+    df_stock_price.set_index('Date', inplace=True)
+
+    return df_stock_price
+
+
 def create_financial_statistics(start_date, end_date, df_stocks, list_of_companies):
     list_of_companies_reversed = list([list_of_companies[1], list_of_companies[0]])
     df_fin_stats = pd.DataFrame(columns=list_of_companies_reversed)
@@ -56,11 +82,6 @@ def create_financial_statistics(start_date, end_date, df_stocks, list_of_compani
     return df_fin_stats
 
 
-def create_stock_graphs(start_date, end_date, df_stock_1, df_stock_2):
-    # Work in progress
-    return 0
-
-
 # ===== initialize the app =====
 app = dash.Dash(__name__)
 
@@ -76,6 +97,10 @@ app.layout = html.Div([
         html.Div([
             dcc.Graph(
                 id='2-stock-subplot'),
+        ]),
+        html.Div([
+            # call generate_html_table
+           # generate_html_table()
         ])
     ]),
     # Stock Ticker Input
@@ -128,31 +153,10 @@ app.css.append_css({
      State("start-date-input", "value"), State("end-date-input", "value")],
 )
 def update_fig(n_clicks, input_stock_1, input_stock_2, input_date_start, input_date_end):
-    df_stocks = pd.DataFrame()
-    data_provider = 'yahoo'
-
-    # Inputted from callback
+    df_stock_price = pull_stock_data(input_stock_1=input_stock_1, input_stock_2=input_stock_2,
+                                     input_date_start=input_date_start,
+                                     input_date_end=input_date_end)
     list_of_companies = [input_stock_2, input_stock_1]
-    start_date = input_date_start
-    end_date = input_date_end
-
-    for company in list_of_companies:
-        df = DataReader(name=company,
-                        data_source=data_provider,
-                        start=start_date,
-                        end=end_date).reset_index()
-        df["ticker"] = company
-        df_stocks = pd.concat([df_stocks, df])
-
-    df_stocks.reset_index(drop=True, inplace=True)
-
-    df_stock_price = pd.pivot(df_stocks, index='Date', columns='ticker', values='Close')
-    df_stock_price = pd.DataFrame(df_stock_price.to_records())
-    df_stock_price.set_index('Date', inplace=True)
-
-    # Call function to create financial statistics to show
-    create_financial_statistics(start_date=start_date, end_date=end_date, df_stocks=df_stock_price,
-                                list_of_companies=list_of_companies)
 
     # ===== Create dash graph objects =====
     trace_stock_1 = go.Scatter(x=list(df_stock_price.index),
@@ -177,6 +181,62 @@ def update_fig(n_clicks, input_stock_1, input_stock_2, input_date_start, input_d
     fig.update_layout(legend_orientation="h")
     return fig
 
+
+# @app.callback(
+#     Output("2-stock-subplot", "figure"),
+#     [Input("stock-submit-button", "n_clicks")],
+#     [State("stock1-input", "value"), State("stock2-input", "value"),
+#      State("start-date-input", "value"), State("end-date-input", "value")],
+# )
+# def generate_html_table(n_clicks, input_stock_1, input_stock_2, input_date_start, input_date_end):
+#     df_stocks = pd.DataFrame()
+#     data_provider = 'yahoo'
+#
+#     # Inputted from callback
+#     list_of_companies = [input_stock_2, input_stock_1]
+#     start_date = input_date_start
+#     end_date = input_date_end
+#
+#     for company in list_of_companies:
+#         df = DataReader(name=company,
+#                         data_source=data_provider,
+#                         start=start_date,
+#                         end=end_date).reset_index()
+#         df["ticker"] = company
+#         df_stocks = pd.concat([df_stocks, df])
+#
+#     df_stocks.reset_index(drop=True, inplace=True)
+#
+#     df_stock_price = pd.pivot(df_stocks, index='Date', columns='ticker', values='Close')
+#     df_stock_price = pd.DataFrame(df_stock_price.to_records())
+#     df_stock_price.set_index('Date', inplace=True)
+#
+#     df_html_table = create_financial_statistics(start_date=start_date, end_date=end_date, df_stocks=df_stock_price,
+#                                                 list_of_companies=list_of_companies)
+#     return html.Div(
+#         [
+#             html.Div(
+#                 html.Table(
+#                     # Header
+#                     [html.Tr([html.Th()])]
+#                     +
+#                     # Body
+#                     [
+#                         html.Tr(
+#                             [
+#                                 html.Td(
+#                                     html.A(
+#                                         ##
+#                                     )
+#                                 )
+#                             ]
+#                         )
+#                     ]
+#                 )
+#             )
+#         ]
+#     )
+#
 
 if __name__ == "__main__":
     app.run_server(debug=True)
